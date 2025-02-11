82a0af966873070b46cfc60fa00657c2c0e3c6f8
BugzeeDev <irvingcsylva@gmail.com>
2025-02-11 15:13:08 -0800

    added tests for rendering components on the missionForm

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -1,7 +1,19 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import React from "react";
 import MissionForm from "./MissionForm"
 
 test('MissionForm renders correctly', () => {
     render(<MissionForm />);
-})
+});
+
+test('renders the message when isFetchingData is true', () => {
+    render(<MissionForm isFetchingData={true} />);
+    const value = screen.queryByText(/we are fetching data/i);
+    expect(value).not.toBeNull();
+});
+
+test('render the button when isFetchingData is false', () => {
+    render(<MissionForm isFetchingData={false} />);
+    const value = screen.queryByRole('button');
+    expect(value).not.toBeNull();
+});
