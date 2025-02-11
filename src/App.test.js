5071765999e1623185a4c3dae19c3f9d00c32710
BugzeeDev <irvingcsylva@gmail.com>
2025-02-11 14:56:15 -0800

    started adding unit tests

---
+++ src/App.test.js
@@ -0,0 +1,24 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+import mockFetchMissions from './api/fetchMissions'
+
+test('app renders without errors', () => {
+    render(<App />)
+});
+
+test('fetches and renders mission data', async () => {
+    render(<App />);
+    mockFetchMissions.mockResolvedValueOnce({
+        data: [
+            {mission_name: "Mission 1", mission_id: "mission 1"},
+            {mission_name: "Mission 2", mission_id: "mission 2"},
+        ]
+    });
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    await wait();
+
+    expect(screen.getAllByTestId("mission")).toHaveLength(2);
+})
